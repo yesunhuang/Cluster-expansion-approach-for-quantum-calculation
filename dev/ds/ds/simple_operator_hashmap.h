afcdb82d35c0c6d41e2843ea254804b524a1a128
Ayajirin <493987054@qq.com>
2020-01-21 15:46:52 +0800

    完成了simple operator的哈希表相关

--- dev/ds/ds/simple_operator_hashmap.h
+++ dev/ds/ds/simple_operator_hashmap.h
@@ -1,5 +1,5 @@
 /**
- * @ 文件名: simple_operator_hashmap.h
+ * @ 文件名: so_hashmap.h
  * @ 功能: 声明单模式operator哈希表的功能
  * @ 作者: AyajiLin
  * @ 邮箱: 493987054@qq.com
@@ -12,7 +12,42 @@
 #define _SIMPLE_OPERATOR_HASHMAP_H_
 
 #include "simple_operator.h"
+#include <malloc.h>
+#include <memory.h>
 
+#define NULL ((void*)0)
+
+typedef struct _SOHashLinkNode {
+	int index;
+	struct _SOHashLinkNode* next;
+} SOHashLinkNode, *pSOHashLinkNode;
+
+typedef struct _SOHashLink {
+	pSOHashLinkNode head;
+	pSOHashLinkNode tail;
+} SOHashLink, *pSOHashLink;
+
+struct _SOHashmap {
+	int capacity;
+	int expect;
+	SOHashLink hashLink;
+	SimpleOperator* data;
+};
+typedef struct _SOHashmap SOHashmap, *pSOHashmap;
+
+int InitSOLink(pSOHashLink link);
+int AddNodeOfSOLink(pSOHashLink link, int indexData);
+int ClearSOLink(pSOHashLink link);
+int MoveSOLink(pSOHashLink srcLink, pSOHashLink dstLink);
+
+int InitSOHashmap(pSOHashmap hm, int expectSize);
+int ClearSOHashmap(pSOHashmap hm);
+int MoveSOHashmap(pSOHashmap srcHm, pSOHashmap dstHm);
+int InsertSOHashmap(pSOHashmap hm, SimpleOperator so);
+
+int _NearestLtEqPrime(int num, int* output);
+int _SOHashGen(SimpleOperator so, int capacity, int* output);
+int _GetDigits(int num, int* output);
 
 
 #endif // !_SIMPLE_OPERATOR_HASHMAP_H_
